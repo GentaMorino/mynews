ad8d8530767b83eeab4db6ba89d6e0634eb140f8
”GentaMorino” <genta.morino@gmail.com>
2019-08-04 11:52:51 +0000

    PHP/Laravel 11 Bladeテンプレートの継承とLaravel Mix

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,1 +1,12 @@
-<h1>profile.create</h1>
+@extends('layouts.profile')
+<!DOCTYPE html>
+@section('title','プロフィール')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+</html>
