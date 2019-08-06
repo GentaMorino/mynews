77c1fb30f92b19901ee2ed2f9ea28897ece0f674
”GentaMorino” <genta.morino@gmail.com>
2019-08-06 06:52:16 +0000

    PHP/Laravel 12 ユーザー認証を実装する

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,1 +1,12 @@
-<h1>profile.edit</h1>
+@extends('layouts.profile')
+<!DOCTYPE html>
+@section('title','プロフィール編集')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール　編集</h2>
+            </div>
+        </div>
+    </div>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix'=>'admin'],function(){
-    Route::get('news/create','Admin\NewsController@add');
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    //news
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    //profile
+    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
